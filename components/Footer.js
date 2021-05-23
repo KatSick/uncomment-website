a6880e3d90227f7f694989b1d0d7a2ed9093d3cd
Ostap Chervak <ostap.chervak@grammarly.com>
2021-05-23 15:00:41 +0300

    Change footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,15 +4,13 @@ import SocialIcon from '@/components/social-icons'
 export default function Footer() {
   return (
     <footer className="divide-y divide-gray-200 dark:divide-gray-700">
-      <div className="flex flex-col items-center mt-16">
-        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
-          <div>{`${new Date().getFullYear()}`}</div>
-        </div>
-        <div className="flex justify-center w-full mb-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} className="w-6 h-6" />
-        </div>
+      <div className="flex items-center mt-16 mb-4">
+        <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} className="mr-2 w-6 h-6" />
+        <span className="flex text-sm space-x-2 text-gray-500 dark:text-gray-400">
+          <p>{siteMetadata.author}</p>
+          <p>{` • `}</p>
+          <p>{`${new Date().getFullYear()}`}</p>
+        </span>
       </div>
     </footer>
   )
